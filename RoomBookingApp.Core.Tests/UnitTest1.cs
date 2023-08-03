3c4a35279c1cb2d561e60afcb0115e1ce09c7f37
Łukasz <lukasz.mentel@ironteam.pl>
2023-08-03 12:21:17 +0200

    4

---
+++ RoomBookingApp.Core.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace RoomBookingApp.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+    }
+}
