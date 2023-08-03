7c085d4182774604715a964b51fe760639b55902
Łukasz Mentel <lukasz199205@gmail.com>
2023-08-03 14:57:39 +0200

    lesson 11

---
+++ RoomBookingApp.Core/Domain/RoomBooking.cs
@@ -0,0 +1,8 @@
+namespace RoomBookingApp.Core.Domain;
+
+public class RoomBooking
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public DateTime Date { get; set; }
+}
