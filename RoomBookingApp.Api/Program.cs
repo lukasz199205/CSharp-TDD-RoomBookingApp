d4dce88c008e70a806e623e0a0e59b478a8e9340
Łukasz Mentel <lukasz199205@gmail.com>
2023-08-04 13:24:57 +0200

    lesson 26

--- RoomBookingApp.Api/Program.cs
+++ RoomBookingApp.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RoomBookingApp.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connString = "DataSource=:memory:";
+var conn = new SqliteConnection(connString);
+conn.Open();
+
+builder.Services.AddDbContext<RoomBookingAppDbContext>(opt => opt.UseSqlite(conn));
 
 var app = builder.Build();
 
